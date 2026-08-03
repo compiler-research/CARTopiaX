e6b4bb5747eb710293a1eb3a53a88a7d1227bad6
salva24 <delatorregonzalezsalvador@gmail.com>
2026-08-03 22:55:19 +0200

    new volumes for tumor cells and cart cells

--- draft/analysis_radius_atribute.py
+++ draft/analysis_radius_atribute.py
@@ -30,10 +30,10 @@ import matplotlib.pyplot as plt
 
 # ---- settings ----
 CSV_PATH = "./output/data_dependent_on_radius_tumor.csv"
-MINUTE = 4320 # change this to the minute you want to plot
+MINUTE = 900 # change this to the minute you want to plot
 # MINUTE = 4320 # change this to the minute you want to plot
 # ATTRIBUTE = "average_oxygen_all_cells_radius"  # change this to the attribute you want to plot
-ATTRIBUTE = "tumor_cells_type5_dead_radius"  # change this to the attribute you want to plot
+ATTRIBUTE = "num_alive_cart_cells_radius"  # change this to the attribute you want to plot
 # DIVIDING_FACTOR = 585  # change this to the factor you want to divide by. Use 525 to pass from mmHg to mol/m3
 DIVIDING_FACTOR = 1  # change this to the factor you want to divide by. Use 525 to pass from mmHg to mol/m3
 # other examples: "average_oncoprotein_radius", "average_oxygen_cancer_cells_radius",
